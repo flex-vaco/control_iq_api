ff1fe65f8f88a5ff0bdc0c0fe7d198b8381bba4d
flex-vaco <137503288+flex-vaco@users.noreply.github.com>
2025-11-05 21:38:09 +0530

    Serve React app and handle UI requests

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ app.use(cors()); // Allow cross-origin requests (from React)
 app.use(express.json({ limit: '50mb' })); // Parse JSON request bodies with increased limit for large imports
 app.use(express.urlencoded({ limit: '50mb', extended: true })); // Support URL-encoded bodies
 
+// Handle all UI requests by serving the React app's index.html
+app.use(express.static('/apps/control_iq/control_iq_ui/build'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/apps/control_iq/control_iq_ui/build', 'index.html')); // Adjust path as needed
+});
+
 //Health check
 app.get('/api/health', (req, res) => {
   const currentTime = new Date();
@@ -24,7 +30,6 @@ app.get('/api/health', (req, res) => {
 });
 
 // Serve static files from uploads directory
-app.use('/ui', express.static('/apps/control_iq/control_iq_ui/build'));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // --- Routes ---
