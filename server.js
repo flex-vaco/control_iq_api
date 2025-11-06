2a83d93969831c943fcead570fdb5bfde457c6a9
flex-vaco <137503288+flex-vaco@users.noreply.github.com>
2025-11-06 11:49:10 +0530

    Change UI route to serve index.html at root

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true })); // Support URL-e
 
 // Handle all UI requests by serving the React app's index.html
 app.use(express.static('/apps/control_iq/control_iq_ui/build'));
-app.get('/ui', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join('/apps/control_iq/control_iq_ui/build', 'index.html')); // Adjust path as needed
 });
 
